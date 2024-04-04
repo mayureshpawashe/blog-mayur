02e6f019e9e010faeedaa043e93e97c7078a0317
mayureshpawashe <mayureshpawashe3036@gmail.com>
2024-04-04 16:22:13 +0530

    backup repo

---
+++ api/index.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
+
+
+
+
+//databse connection 
+dotenv.config();
+mongoose.connect('mongodb+srv://mayur:mayur@mern-blog.wo8zrga.mongodb.net/mern-blog?retryWrites=true&w=majority&appName=mern-blog').then(() => {
+    console.log('MongoDb is connected');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+
+
+
+const app = express();
+app.use(express.json());
+
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000!');
+  });
+
+
+
+  app.use('/api/user', userRoutes);
+  app.use('/api/auth',authRoutes);
+
+
+  app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    res.status(statusCode).json({
+      success: false,
+      statusCode,
+      message,
+    });
+  });
---
+++ client/src/App.jsx
@@ -0,0 +1,29 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Search from './pages/Search'
+import Header from './components/Header';
+import Projects from './pages/Projects';
+
+
+export default function App() {
+  return (
+
+    <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/sign-in' element={<SignIn />} />
+      <Route path='/sign-up' element={<SignUp />} />
+      <Route path='/search' element={<Search />} />
+      <Route path='/projects' element={<Projects />} />
+
+
+    </Routes>
+    </BrowserRouter>
+
+  );
+}
