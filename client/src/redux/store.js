4b5f6e76ef9e7663dc1c6135b98267101a399d7e
mayureshpawashe <mayureshpawashe3036@gmail.com>
2024-04-11 19:09:33 +0530

    added redux toolkit

---
+++ client/src/redux/store.js
@@ -0,0 +1,7 @@
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import userReducer from './user/userSlice';
+export const store = configureStore({
+    reducer:{
+        user:userReducer,
+    },
+})
