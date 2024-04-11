fe65a92fab1313a581789f07898ca28fbd7cb64c
mayureshpawashe <mayureshpawashe3036@gmail.com>
2024-04-11 15:21:40 +0530

    sign in  added

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,12 @@
 import react from "react"
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import { access } from "fs";
+import { errorHandler } from '../utils/error.js';
+
+//signup api
 export const signup= async(req,res,next)  =>{
     const { username, email, password } = req.body;
     if (
@@ -29,6 +35,37 @@ export const signup= async(req,res,next)  =>{
       {
        next(error);
       }
-
-
 };
+
+//sign in API
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  
+
+  if (!email || !password || email === '' || password === '') {
+    next(errorHandler(400, 'All fields are required!!!!'));
+  }
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, 'User not found'));
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) 
+    {
+      return next(errorHandler(400, 'Invalid Password'));
+    }
+
+    const token =jwt.sign(
+    { id:validUser._id }, process.env.JWT_SECRET);
+
+    const {password:pass, ...rest}=validUser._doc;
+    res.status(200).cookie('access_token',token,{
+      httpOnly:true}).json(rest);
+} catch (error) {
+    next(error);
+  }
+};
+
+
