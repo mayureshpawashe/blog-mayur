0b5718905e2290ef14c057db7f8761c3f5e035b6
mayureshpawashe <mayureshpawashe3036@gmail.com>
2024-04-26 19:59:53 +0530

    dark mode added

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,13 +3,15 @@ import { Link, useHref, useLocation, useNavigate } from 'react-router-dom';
 import { AiOutlineSearch } from 'react-icons/ai';
 import { FaMoon, FaSun } from 'react-icons/fa';
 import { useSelector, useDispatch } from 'react-redux';
-//import { toggleTheme } from '../redux/theme/themeSlice';
+import { toggleTheme } from '../redux/theme/themeSlice';
 //import { signoutSuccess } from '../redux/user/userSlice';
 import { useEffect, useState } from 'react';
 
 export default function Header() {
   const path = useLocation().pathname;
+  const dispatch = useDispatch();
   const { currentUser } = useSelector((state) => state.user);
+  const { theme } = useSelector((state) => state.theme);
   return (
     <Navbar className='border-b-2'> Header
     <Link
@@ -37,8 +39,8 @@ export default function Header() {
       </Button>
 
       <div className='flex gap-2 md:order-2' >
-        <Button className='w-12 h-10 hidden sm:inline' color='gray' pill >
-          <FaMoon> </FaMoon>
+        <Button className='w-12 h-10 hidden sm:inline' color='gray' pill onClick={()=>dispatch(toggleTheme())} >
+        {theme === 'light' ? <FaSun /> : <FaMoon />}
         </Button>
 
 
