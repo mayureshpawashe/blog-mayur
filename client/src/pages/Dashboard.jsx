84c88e92aa4878c7112cca1b22798bc5263698de
mayureshpawashe <mayureshpawashe3036@gmail.com>
2024-05-28 14:25:06 +0530

    show users to the admin dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -3,6 +3,7 @@ import { useLocation } from 'react-router-dom';
 import DashSidebar from '../components/DashSidebar';
 import DashProfile from '../components/DashProfile';
 import DashPosts from '../components/DashPosts';
+import DashUsers from '../components/DashUsers';
 export default function Dashboard() {
   const location = useLocation();
   const [tab, setTab] = useState('');
@@ -29,8 +30,13 @@ export default function Dashboard() {
 
       {/* profile */}
       {tab === 'profile' && <DashProfile />}
-       {/* posts... */}
-       {tab === 'posts' && <DashPosts />}
-    </div>
+
+      {/* posts... */}
+      {tab === 'posts' && <DashPosts />}
+
+      {/* users */}
+      {tab === 'users' && <DashUsers />}
+
+      </div>
   )
 }
