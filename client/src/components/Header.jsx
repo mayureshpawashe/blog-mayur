75f74e450b84c73f26e1822fed9d323f7a43928b
mayureshpawashe <mayureshpawashe3036@gmail.com>
2024-06-02 13:51:28 +0530

    complete the home page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -28,7 +28,7 @@ export default function Header() {
     }
   };
   return (
-    <Navbar className='border-b-2'> Header
+    <Navbar className='border-b-2'> 
     <Link
         to='/'
         className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'
