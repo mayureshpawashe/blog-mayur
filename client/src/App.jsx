8641b3987325042c67a5037b2d7eee0c08bc439e
mayureshpawashe <mayureshpawashe3036@gmail.com>
2024-06-02 14:20:26 +0530

    complete the search option

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,6 +15,7 @@ import UpdatePost from './pages/UpdatePost';
 import PostPage from './pages/PostPage';
 import ScrollToTop from './components/ScrollToTop';
 
+
 export default function App() {
   return (
 
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react';
-import { Link, useHref, useLocation, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { AiOutlineSearch } from 'react-icons/ai';
 import { FaMoon, FaSun } from 'react-icons/fa';
 import { useSelector, useDispatch } from 'react-redux';
@@ -9,9 +9,21 @@ import { useEffect, useState } from 'react';
 
 export default function Header() {
   const path = useLocation().pathname;
+  const location = useLocation();
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const { currentUser } = useSelector((state) => state.user);
   const { theme } = useSelector((state) => state.theme);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
+
   const handleSignout = async () => {
     try {
       const res = await fetch('/api/user/signout', {
@@ -27,79 +39,79 @@ export default function Header() {
       console.log(error.message);
     }
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
   return (
-    <Navbar className='border-b-2'> 
-    <Link
+    <Navbar className='border-b-2'>
+      <Link
         to='/'
         className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'
       >
-        <span className='px-2 py-1 bg-gradient-to-r from-orange-500 via-blue-500 to-green-500 rounded-lg text-white'>
+        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
           Mayuresh's Blog
         </span>
         
       </Link>
-
-      <form>
+      <form onSubmit={handleSubmit}>
         <TextInput
-              type='text'
-              placeholder='Search...'
-              rightIcon={AiOutlineSearch}
-              className='hidden lg:inline'
+          type='text'
+          placeholder='Search...'
+          rightIcon={AiOutlineSearch}
+          className='hidden lg:inline'
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
-
       </form>
-      
       <Button className='w-12 h-10 lg:hidden' color='gray' pill>
         <AiOutlineSearch />
       </Button>
-
-      <div className='flex gap-2 md:order-2' >
-        <Button className='w-12 h-10 hidden sm:inline' color='gray' pill onClick={()=>dispatch(toggleTheme())} >
-        {theme === 'light' ? <FaSun /> : <FaMoon />}
-        </Button>
-
-
-      {currentUser ? (
-        <Dropdown
-        arrowIcon={false}
-        label={
-          <Avatar
-          alt='user'
-          rounded
-          />
-        }
+      <div className='flex gap-2 md:order-2'>
+        <Button
+          className='w-12 h-10 hidden sm:inline'
+          color='gray'
+          pill
+          onClick={() => dispatch(toggleTheme())}
         >
-          <Dropdown.Header>
-          <span className='block text-sm'>@{currentUser.username}</span>
-          <span className='block text-sm font-medium truncate'>
+          {theme === 'light' ? <FaSun /> : <FaMoon />}
+        </Button>
+        {currentUser ? (
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar alt='user' img={currentUser.profilePicture} rounded />
+            }
+          >
+            <Dropdown.Header>
+              <span className='block text-sm'>@{currentUser.username}</span>
+              <span className='block text-sm font-medium truncate'>
                 {currentUser.email}
               </span>
-          </Dropdown.Header>
-          <Link to={'/dashboard?tab=profile'}>
+            </Dropdown.Header>
+            <Link to={'/dashboard?tab=profile'}>
               <Dropdown.Item>Profile</Dropdown.Item>
             </Link>
             <Dropdown.Divider />
-            <Dropdown.Item  onClick={handleSignout} >Sign out</Dropdown.Item>
-          
-        </Dropdown>
-
-      ):
-      (
-        <Link to='/sign-in'>
-          <Button gradientDuoTone='purpleToBlue' outline>
-            Sign-In
-          </Button>
-        </Link>
-      )
-      }
-
-
-      <Navbar.Toggle/>
+            <Dropdown.Item onClick={handleSignout}>Sign out</Dropdown.Item>
+          </Dropdown>
+        ) : (
+          <Link to='/sign-in'>
+            <Button gradientDuoTone='purpleToBlue' outline>
+              Sign In
+            </Button>
+          </Link>
+        )}
+        <Navbar.Toggle />
       </div>
-
-
       <Navbar.Collapse>
-      <Navbar.Link active={path === '/'} as={'div'}>
+        <Navbar.Link active={path === '/'} as={'div'}>
           <Link to='/'>Home</Link>
         </Navbar.Link>
         <Navbar.Link active={path === '/about'} as={'div'}>
@@ -110,7 +122,5 @@ export default function Header() {
         </Navbar.Link>
       </Navbar.Collapse>
     </Navbar>
-
-    
   );
 }
